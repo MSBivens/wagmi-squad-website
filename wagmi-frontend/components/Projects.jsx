ef818adada76cf7dd0c240493487a49141a024bd
MSBivens <mikebiv95@gmail.com>
2022-07-13 19:10:10 -0500

    project components built

---
+++ wagmi-frontend/components/Projects.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import Placeholder from "../public/assets/placeholder.PNG";
+
+const Projects = () => {
+  return (
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <h2 className="py-4">What We&apos;ve Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="WAGMI Squad DAO"
+            backgroundImg={Placeholder}
+            projectUrl="/"
+          />
+          <ProjectItem
+            title="WAGMI Squad Access Token"
+            backgroundImg={Placeholder}
+            projectUrl="/"
+          />
+          <ProjectItem
+            title="WAGMI Squad Reputation Token"
+            backgroundImg={Placeholder}
+            projectUrl="/"
+          />
+          <ProjectItem
+            title="WAGMI Squad Website"
+            backgroundImg={Placeholder}
+            projectUrl="/"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
--- wagmi-frontend/pages/index.js
+++ wagmi-frontend/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Main from "../components/Main";
+import Projects from "../components/Projects";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main />
+      <Projects />
     </div>
   );
 }
